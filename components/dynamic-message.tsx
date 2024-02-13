dfd8c423145b469379423978dd480761baa7a99f
Vurak <vurak.of.sulcan@gmail.com>
2024-02-13 19:20:26 +0100

    suppressHydrationWarning added as server rendered wasn't matching client side

--- components/dynamic-message.tsx
+++ components/dynamic-message.tsx
@@ -11,7 +11,10 @@ const messages = [
 export default function DynamicMessage() {
   const message = messages.at(Math.floor(Math.random() * messages.length))
   return (
-    <h3 className="text-xl sm:text-3xl md:text-3xl lg:text-5xl font-bold text-center">
+    <h3
+      className="text-xl sm:text-3xl md:text-3xl lg:text-5xl font-bold text-center"
+      suppressHydrationWarning
+    >
       {message}
     </h3>
   )
